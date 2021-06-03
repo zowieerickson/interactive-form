0115c6f4e6a33265e812a24ed978d34e456e43fc
Zowie Erickson <zowie.erickson@gmail.com>
2021-06-03 18:34:45 -0400

    feat: first input field automatically focused on load

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Selectors
+const name = document.querySelector("#name");
+const jobRole = document.querySelector("#title");
+const otherJobRole = document.querySelector("#other-job-role");
+
+// Intros
+name.focus();
