3a591453a15a9e9541f583619693322b95e72653
Zowie Erickson <zowie.erickson@gmail.com>
2021-06-03 18:37:20 -0400

    feat: "Other job role?" input only displays when select option value is "Other", else it will be hidden

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const otherJobRole = document.querySelector("#other-job-role");
 
 // Intros
 name.focus();
+otherJobRole.style.display = "none";
+
+jobRole.addEventListener("change", (e) => {
+  if (jobRole.value == "other") {
+    otherJobRole.style.display = "";
+  } else if (jobRole.value !== "other") {
+    otherJobRole.style.display = "none";
+  }
+});
