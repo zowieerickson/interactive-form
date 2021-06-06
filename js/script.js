057cf3a7b1988a9e693169eb6211e9200e08395a
Zowie Erickson <zowie.erickson@gmail.com>
2021-06-05 20:20:57 -0400

    update: begin t-shirt drop down feat

--- js/script.js
+++ js/script.js
@@ -2,15 +2,32 @@
 const name = document.querySelector("#name");
 const jobRole = document.querySelector("#title");
 const otherJobRole = document.querySelector("#other-job-role");
+const color = document.querySelector("#color");
+const design = document.querySelector("#design");
+const jsPuns = document.querySelectorAll("[data-theme='js puns']");
+console.log(jsPuns);
 
 // Intros
 name.focus();
 otherJobRole.style.display = "none";
+// color.disabled = true;
 
-jobRole.addEventListener("change", (e) => {
+jobRole.addEventListener("change", () => {
   if (jobRole.value == "other") {
     otherJobRole.style.display = "";
   } else if (jobRole.value !== "other") {
     otherJobRole.style.display = "none";
+  }
+});
+
+design.addEventListener("change", (e) => {
+  color.disabled = false;
+  if (design.value.includes("puns")) {
+    for (let i = 0; i < jsPuns.length; i++) {
+      jsPuns[i].style.display = "hidden";
+      console.log(jsPuns[i]);
+      // design.remove(jsPuns[i]);
+      console.log("selected puns");
+    }
   }
 });
